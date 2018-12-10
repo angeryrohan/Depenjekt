cee95c05d47cb4ad2a541509919787e18a5d27de
Rohan Singh <rohan.clevduct@gmail.com>
2018-12-10 14:37:20 +0530

    Initialising the project

--- src/depenject.js
+++ src/depenject.js
@@ -5,19 +5,3 @@ let fs = require('fs');
 let addDepenencies = (input) => {
     return input;
 }
-
-// Generates output file with injected dependencies
-let generate = (input) => {
-    output = '';
-    input = flatten(input);
-    input.forEach(element => {
-        output += createRequire(element);
-    });
-    output += "\nlet stub = () => { \n"
-    output += tryCatchFunc(input);
-    output += "\n};";
-    output += "\nmodule.exports = stub;";
-    let writeStream = fs.createWriteStream('stub.js');
-    writeStream.write(output);
-    writeStream.end();
-};
